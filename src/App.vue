9b901c1e221d3bc28ca483298ae74306ed98f3c8
GalaDriel <g_ala@naver.com>
2022-12-15 19:50:55 +0900

    Upate: Components - Common - NoteLoading

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
             <RouterView :key="$route.path" />
         </div>
         <NoteFooter />
+        <NoteLoading />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NoteFooter from './components/common/NoteFooter.vue';
+import NoteLoading from './components/common/NoteLoading.vue';
 
 export default defineComponent({
     components: {
         NoteFooter,
+        NoteLoading,
     },
     setup() {
         return {};
---
+++ src/components/common/NoteLoading.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="loadingWrap" v-if="loading">
+        <div class="loader"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import { useCommon } from '@/store/commonStore';
+import { storeToRefs } from 'pinia';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const common = useCommon();
+        const { loading } = storeToRefs(common);
+
+        return {
+            loading,
+        };
+    },
+});
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/components/common/NoteLoading.scss';
+</style>
