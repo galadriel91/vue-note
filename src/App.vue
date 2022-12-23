cf8e4fb433ed72486b225143515dfd80f8c17442
GalaDriel <g_ala@naver.com>
2022-12-23 18:25:53 +0900

    Update: Add home btn in NoteSearch

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ export default defineComponent({
         };
 
         const backgroundImageInlineStyle = computed(() => {
-            return `background-image: url("./assets/${status.value}bg${randomBg.value}.jpg")`;
+            return `background-image: url("/assets/${status.value}bg${randomBg.value}.jpg")`;
         });
 
         onMounted(() => {
--- src/components/common/NoteSearch.vue
+++ src/components/common/NoteSearch.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
+        <button
+            class="material-symbols-outlined homeBtn"
+            @click="onClickMain"
+            v-if="isHome"
+        >
+            home
+        </button>
         <form @submit.prevent="onSubmitForm">
             <input
+                :class="{ isSearch: isHome }"
                 type="text"
                 placeholder="검색 키워드를 입력해 주세요"
                 v-model="searchValue"
@@ -14,12 +22,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { defineComponent, ref, computed } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
 export default defineComponent({
     setup() {
         const router = useRouter();
+        const route = useRoute();
         const searchValue = ref('');
 
         const onSubmitForm = () => {
@@ -31,9 +40,19 @@ export default defineComponent({
             }
         };
 
+        const onClickMain = () => {
+            router.push('/');
+        };
+
+        const isHome = computed(() => {
+            return route.name === 'search';
+        });
+
         return {
             searchValue,
             onSubmitForm,
+            onClickMain,
+            isHome,
         };
     },
 });
